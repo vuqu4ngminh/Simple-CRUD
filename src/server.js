ad862eb4042efc20b989c4ff9883194022569e6a
vuqu4ngminh <solution.vuqu4ngminh@gmail.com>
2023-12-16 19:19:22 +0700

    Allow all origin

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         res.setHeader('Access-Control-Allow-Credentials', true);
